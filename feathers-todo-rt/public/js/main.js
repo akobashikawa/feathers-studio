82f46412c8ef77c8eadffda79ddf1c7e65de9ff3
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-05-19 21:48:14 -0500

    Fix vue methods in order to support rt

--- feathers-todo-rt/public/js/main.js
+++ feathers-todo-rt/public/js/main.js
@@ -6,17 +6,17 @@ socket.on('everybody', function (message) {
 
 socket.on('api/todo created', task => {
     console.log('created', task);
-    app.getTasks();
+    app._addTask(null, task);
 });
 
 socket.on('api/todo updated', task => {
     console.log('updated', task);
-    // app.getTasks();
+    app._updateTask(null, task);
 });
 
-socket.on('api/todo deleted', task => {
-    console.log('deleted', task);
-    // app.getTasks();
+socket.on('api/todo removed', task => {
+    console.log('removed', task);
+    app._removeTask(null, task);
 });
 
 var app = new Vue({
@@ -44,6 +44,16 @@ var app = new Vue({
                 })
                 .catch(err => console.log(err));
         },
+        getTask: function (id) {
+            const url = `/api/todo/${id}`;
+            fetch(url)
+                .then(res => res.json())
+                .then(task => {
+                    let taskToUpdate = this.tasks.find(item => item.id === task.id);
+                    taskToUpdate = Object.assign(taskToUpdate, task);
+                })
+                .catch(err => console.log(err));
+        },
         toogleStatus: function (task) {
             switch (task.status) {
                 case "todo":
@@ -58,110 +68,57 @@ var app = new Vue({
             }
             this.updateTask(task);
         },
-        addTask: function (newTask) {
-            const description = newTask && newTask.trim();
+        _addTask: function(error, task) {
+            if (error) {
+                throw error;
+            }
+            this.tasks.push(task);
+            this.newTask = '';
+            this.$refs['newTask'].focus();
+        },
+        addTask: function (text) {
+            const description = text && text.trim();
             if (!description) {
                 return;
             }
-
-            // const url = `/api/todo`;
-            // const headers = {
-            //     Accept: "application/json, text/plain, */*",
-            //     "Content-Type": "application/json"
-            // };
             const data = {
                 description,
                 status: 'todo'
             }
-            socket.emit('create', '/api/todo', data, (error, result) => {
-                if (error) {
-                    throw error;
-                }
-                // socket.emit('find', '/api/todo', (error, messageList) => {
-                //     if (error) throw error
-                //     console.log('Current tasks', messageList);
-                // });
-                // const task = Object.assign(data, result);
-                // this.tasks.push(task);
-                this.newTask = '';
-                this.$refs['newTask'].focus();
-            });
-            // fetch(url, {
-            //     method: 'POST',
-            //     headers: new Headers(headers),
-            //     body: JSON.stringify(data)
-            // })
-            //     .then(res => res.json())
-            //     .then(result => {
-            //         console.log('created');
-            //         this.tasks.push(Object.assign(data, result));
-            //         console.log(this.tasks);
-            //         this.newTask = '';
-            //         this.$refs['newTask'].focus();
-            //     })
-            //     .catch(err => console.log(err));
+            socket.emit('create', '/api/todo', data, null);
         },
-        deleteTask: function (task) {
-            if (!confirm('Delete task: ' + task.description.substr(0, 14))) {
-                return;
+        _updateTask: function (error, task) {
+            if (error) {
+                throw error;
             }
-            const id = task.id;
-            // const url = `/api/todo/${id}`;
-            socket.emit('delete', '/api/todo', {id}, (error, result) => {
-                if (error) {
-                    throw error;
-                }
-            });
-            // fetch(url, {
-            //     method: 'DELETE'
-            // })
-            //     .then(res => res.json())
-            //     .then(result => {
-            //         console.log('deleted');
-            //         this.tasks = this.tasks.filter(item => item.id !== id);
-            //     })
-            //     .catch(err => console.log(err));
+            let taskToUpdate = this.tasks.find(item => item.id === task.id);
+            taskToUpdate = Object.assign(taskToUpdate, task);
         },
         updateTask: function (task) {
             const id = task.id;
 
-            // const url = `/api/todo/${id}`;
-            // const headers = {
-            //     Accept: "application/json, text/plain, */*",
-            //     "Content-Type": "application/json"
-            // };
             const data = {
                 id,
                 description: task.description,
                 status: task.status
             };
-            socket.emit('update', '/api/todo', data, (error, result) => {
-                if (error) {
-                    throw error;
-                }
-            });
-            // fetch(url, {
-            //     method: 'PUT',
-            //     headers: new Headers(headers),
-            //     body: JSON.stringify(data)
-            // })
-            //     .then(res => res.json())
-            //     .then(result => {
-            //         console.log('updated');
-            //     })
-            //     .catch(err => console.log(err));
+            socket.emit('update', '/api/todo', id, data, null);
+        },
+        _removeTask: function (error, id) {
+            if (error) {
+                throw error;
+            }
+            this.tasks = this.tasks.filter(item => item.id !== id);
+        },
+        removeTask: function (task) {
+            if (!confirm('Remove task: ' + task.description.substr(0, 14))) {
+                return;
+            }
+            const id = task.id;
+            socket.emit('remove', '/api/todo', id, null);
         },
         cancelEditTask: function (task) {
-            const id = task.id;
-            const url = `/api/todo/${id}`;
-            fetch(url)
-                .then(res => res.json())
-                .then(result => {
-                    console.log('cancel edit');
-                    let theTask = this.tasks.find(item => item.id == id);
-                    theTask = Object.assign(theTask, result);
-                })
-                .catch(err => console.log(err));
+            this.getTask(task.id);
         },
     }
 });
